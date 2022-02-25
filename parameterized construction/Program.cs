171bc32c8bc911b9cb783475e6998bdc2a73b3be
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-25 12:46:46 +0530

    Add project files.

---
+++ parameterized construction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Demoproject
+{
+    class Parameterizedcondemo
+    {
+        int x;
+        public Parameterizedcondemo(int i)
+        {
+            x = i;
+            Console.WriteLine("Parameterized constructor is called:" + i);
+        }
+        public void display()
+        {
+            Console.Write("Value of x is:" +x);
+        }
+        static void Main()
+        {
+            Parameterizedcondemo cd1 = new Parameterizedcondemo(10);
+            Parameterizedcondemo cd2 = new Parameterizedcondemo(15);
+            Parameterizedcondemo cd3 = new Parameterizedcondemo(20);
+            cd1.display();
+            cd2.display();
+            Console.ReadLine();
+        }
+    }
+}
